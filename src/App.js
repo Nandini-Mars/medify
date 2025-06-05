cfba5598e6f7cdca9d5d6db25539215fd68ca8f3
Nandini-Mars <nandini.mailbox@gmail.com>
2025-06-05 17:17:31 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
-import './App.css';
-import HeroSection from './components/HeroSection/HeroSection';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter } from 'react-router-dom';
-import Home from './Home/Home'
-import DownloadApp from './components/Sections/DownloadApp/DownloadApp';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./Home/Home";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
+import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 
